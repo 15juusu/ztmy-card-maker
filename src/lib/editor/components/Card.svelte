a1493d040d28b44aea718937eba57c44122d90f2
15juusu <194628055+15juusu@users.noreply.github.com>
2025-01-26 14:32:21 -0500

    feat: add Card component and update page layout

---
+++ src/lib/editor/components/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  let {
+    element
+  }: {
+    element: 'darkness' | 'electric' | 'flame' | 'wind';
+  } = $props();
+
+  let getBgUrl = (): string => {
+    return `/images/bg-card-element-${element}.jpg`;
+  };
+</script>
+
+<div class="container" style:background-image={`url(${getBgUrl()})`}></div>
+
+<style>
+  .container {
+    aspect-ratio: 700 / 977;
+    background-color: white;
+    background-position: center;
+    background-size: cover;
+    height: auto;
+    max-height: 100%;
+    max-width: 512px;
+    width: 100%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,43 @@
 <script lang="ts">
-  import Foo from '$editor/components/Foo.svelte';
+  import Card from '$editor/components/Card.svelte';
 </script>
 
-<Foo name="world" />
+<div class="container">
+  <div class="column">
+    <Card element="wind" />
+  </div>
+  <div class="column">
+    <h1>ZUTOMAYO</h1>
+  </div>
+</div>
+
+<style>
+  :global(body) {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  .container {
+    background-color: #e6e9f1;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    height: 100dvh;
+  }
+
+  .column {
+    align-items: center;
+    box-sizing: border-box;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    padding: 2rem;
+  }
+
+  @media (min-width: 768px) {
+    .container {
+      flex-direction: row;
+    }
+  }
+</style>
