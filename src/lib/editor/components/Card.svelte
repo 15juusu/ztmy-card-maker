1106cd821e7240d06277246a10768c668e8a27e6
15juusu <194628055+15juusu@users.noreply.github.com>
2025-02-02 18:50:45 -0500

    feat: enhance Card component with responsive scaling and background image

--- src/lib/editor/components/Card.svelte
+++ src/lib/editor/components/Card.svelte
@@ -1,16 +1,39 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   let {
     element
   }: {
     element: 'darkness' | 'electric' | 'flame' | 'wind';
   } = $props();
 
+  let container: HTMLDivElement;
+  let scaleUnit: number = $state(0);
+
+  let setScaleUnit = () => {
+    scaleUnit = container.offsetWidth * 0.05;
+  };
+
   let getBgUrl = (): string => {
     return `/images/bg-card-element-${element}.jpg`;
   };
+
+  onMount(() => {
+    setScaleUnit();
+    window.addEventListener('resize', setScaleUnit);
+
+    return () => {
+      window.removeEventListener('resize', setScaleUnit);
+    };
+  });
 </script>
 
-<div class="container" style:background-image={`url(${getBgUrl()})`}></div>
+<div
+  class="container"
+  style:background-image={`url(${getBgUrl()})`}
+  style:border-radius={`${scaleUnit}px`}
+  bind:this={container}
+></div>
 
 <style>
   .container {
